c496aaac7e683b897e8a6eb4bb69fe3b9abd24ec
Aravind Chinna <151151715+aravi143@users.noreply.github.com>
2023-12-11 15:54:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let first = parseInt(prompt());
+let result = first * 2;
+let second = parseInt(prompt());
+result += second;
+alert("The final result is: " + result);
